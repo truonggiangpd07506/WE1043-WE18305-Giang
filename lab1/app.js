e7dca996cbe2ec041f56b40397d4562c4a7d55a2
truonggiangpd07506 <gianglvtpd07506@fpt.edu.vn>
2023-03-08 14:39:56 +0700

    nop lab 1

---
+++ lab1/app.js
@@ -0,0 +1,9 @@
+let a = prompt (" moi nhap so nguyen a");
+let a1 = parseInt(a);
+
+let b = prompt(" moi nhap so nguyen b");
+let b1 = parseInt(b);
+
+let c = parseInt(a)+ parseInt(b);
+console.log(c);
+document.write("ket qua : "+c)
